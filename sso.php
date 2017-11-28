c4dc4101f72afa1a00c022b349fd9a6a5698ee5e
jazinheira <jazinheira@users.noreply.github.com>
2017-11-28 14:10:43 -0500

    Fix comment formatting

--- sso.php
+++ sso.php
@@ -47,13 +47,13 @@ if (isset($SESSION->wantsurl) && !isset($wantsurl)) {
     $wantsurl = urlencode($SESSION->wantsurl);
 }
 
-//Build wantsurl
+// Build wantsurl.
 $redirecturl = $PAGE->url . '?sso=' . $payload . '&sig=' . $signature;
 if (isset($wantsurl)) {
     $redirecturl = $redirecturl . '&wantsurl=' . urlencode($SESSION->wantsurl);
 }
 
-// Set wantsurl so user is redirected back here on login
+// Set wantsurl so user is redirected back here on login.
 $SESSION->wantsurl =  $redirecturl;
 
 // Check if user is logged and not guest. If not, redirect to login page.
@@ -62,7 +62,7 @@ if (isguestuser()) {
 }
 require_login(null, false);
 
-// Reset $SESSION->wantsurl
+// Reset $SESSION->wantsurl.
 if (!isset($wantsurl)) {
     unset($SESSION->wantsurl);
 }
@@ -75,9 +75,9 @@ $ssohelper = new Cviebrock\DiscoursePHP\SSOHelper();
 
 $ssohelper->setSecret($CFG->discoursesso_secret_key);
 
-// validate the payload
+// Validate the payload.
 if (!($ssohelper->validatePayload($payload, $signature))) {
-    // invaild, deny
+    // invalid, deny.
     header("HTTP/1.1 403 Forbidden");
     echo("Bad SSO request");
     $exceptionparam = new stdClass;
@@ -89,16 +89,16 @@ if (!($ssohelper->validatePayload($payload, $signature))) {
 
 $nonce = $ssohelper->getNonce($payload);
 
-// Required and must be unique to your application
+// Required and must be unique to your application.
 $userid = $USER->id;
 
-// Required and must be consistent with your application
+// Required and must be consistent with your application.
 $useremail = $USER->email;
 
 // Optional - if you don't set these, Discourse will generate suggestions
-// based on the email address
+// based on the email address.
 
-// Get the user's description
+// Get the user's description.
 $user = $DB->get_record('user', array('id' => $USER->id));
 $userdescription = format_text($user->description, $user->descriptionformat);
 
@@ -115,12 +115,12 @@ $userpicture->size = 1; // Size f1.
 if (($userpicture->user->picture > 0) || !empty($CFG->enablegravatar)) {
     $useravatar = $userpicture->get_url($PAGE)->out(false);
 }
-// Add the avatar if set
+// Add the avatar if set.
 if (isset($useravatar)) {
     $extraparams['avatar_url'] = $useravatar;
 }
 
-// build query string and redirect back to the Discourse site
+// Build query string and redirect back to the Discourse site.
 $query = $ssohelper->getSignInString($nonce, $userid, $useremail, $extraparams);
 $url = $CFG->discoursesso_discourse_url . '/session/sso_login?' . $query;
 redirect($url);

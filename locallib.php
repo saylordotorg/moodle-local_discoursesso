e55506d3982fa5104cf4ad6ab250141a5ba1cb08
jazinheira <jazinheira@users.noreply.github.com>
2020-04-24 16:39:42 -0400

    Add response http code and error in output for createGroup()

--- locallib.php
+++ locallib.php
@@ -129,7 +129,7 @@ function discoursesso_add_group($cohortid) {
     $r = $api->createGroup(clean_name($ssocohort->cohortname));
 
     if ($r->http_code != 200) {
-        echo $OUTPUT->notification(get_string('errorcreategroupdiscourse', 'local_discoursesso', clean_name($ssocohort->cohortname)), \core\output\notification::NOTIFY_WARNING);
+        echo $OUTPUT->notification(get_string('errorcreategroupdiscourse', 'local_discoursesso', clean_name($ssocohort->cohortname))."<br>Response: [".$r->http_code."] ".$r->apiresult->errors[0], \core\output\notification::NOTIFY_WARNING);
         return false;
     }
 

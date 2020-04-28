54458e407bfa7fde321f0d969bf05d0d21318d23
jazinheira <jazinheira@users.noreply.github.com>
2020-04-28 13:11:18 -0400

    Downgrade release version to 1.4.0

--- version.php
+++ version.php
@@ -30,5 +30,5 @@ $plugin->requires = 2016052306;  // Requires this Moodle version - at least 3.1
 $plugin->cron     = 0;
 $plugin->component = 'local_discoursesso';
 
-$plugin->release = '2.0.0';
+$plugin->release = '1.4.0';
 $plugin->maturity = MATURITY_STABLE;
